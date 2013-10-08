b1b3fe5dea570dcea7099b105bc910b05d6cd8fb
wisedulab2 <yangyr.aaron@gmail.com>
2013-10-08 14:22:25 +0800

    update

--- redis/install.sh
+++ redis/install.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 redis_url=http://download.redis.io/releases/redis-2.6.16.tar.gz
-redis_version=redis-2.6.16-tc
+malloc_version=tc
+redis_version=redis-2.6.16-$malloc_version
 deploy_dir=/usr/local/bin/$redis_version
 home_path=$PWD
 config_dir=/etc/redis
@@ -28,17 +29,17 @@ sudo cp {redis-benchmark,redis-check-aof,redis-check-dump,redis-cli,redis-sentin
 echo "configuring ..."
 cd $home_path/$redis_version
 
-if [[ ! -d config_dir ]]; then
-	sudo mkdir -p $config_dir
-fi
-sudo cp redis.conf $config_dir/$port.conf
+# if [[ ! -d config_dir ]]; then
+# 	sudo mkdir -p $config_dir
+# fi
+# sudo cp redis.conf $config_dir/$port.conf
 
-if [[ ! -d  data_dir ]]; then
-	sudo mkdir -p $data_dir
-fi
+# if [[ ! -d  data_dir ]]; then
+# 	sudo mkdir -p $data_dir
+# fi
 
-if [[ ! -d log_dir ]]; then
-	sudo mkdir -p $log_dir
-fi
+# if [[ ! -d log_dir ]]; then
+# 	sudo mkdir -p $log_dir
+# fi
 
 sudo cp utils/redis_init_script /etc/init.d/redis_$port
