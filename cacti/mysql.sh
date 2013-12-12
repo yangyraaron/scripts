76105640eee30e4fd5b7a431cfeea0433cf09347
aaron yang <yangyr.aaron@gmail.com>
2013-12-12 17:01:34 +0800

    add mysql plugin into cacti

---
+++ cacti/mysql.sh
@@ -0,0 +1,13 @@
+#！/bin/bash
+wget http://mysql-cacti-templates.googlecode.com/files/better-cacti-templates-1.1.8.tar.gz
+tar zxvf better-cacti-templates-1.1.8.tar.gz
+cd better-cacti-templates-1.1.8
+cp scripts/ss_get_mysql_stats.php /usr/share/cacti/site/scripts
+
+sudo vi /usr/share/cacti/site/scripts/ss_get_mysql_stats.php
+$mysql_user = ''
+$mysql_password=''
+$mysql_port=3306
+
+#test
+#php -q ./ss_get_mysql_stats.php --host 10.88.1.134 --items cv,cx,cy,cz --user cacti --pass cacti --port 3306
